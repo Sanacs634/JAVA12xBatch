6bd2c86d59cbbd167c1bfa0fabc9b37b218da750
SANA C S <sanacs786@gmail.com>
2025-06-17 22:46:18 +0530

    commit 17-06-2025

--- src/Task/Task003.java
+++ src/Task/Task003.java
@@ -2,7 +2,7 @@ package Task;
 //The ternary operator and let me know if I give you a number. Let me know if it is even or odd.
 public class Task003 {
     public static void main(String[] args) {
-    int num =0;
+    int num =7;
     String Is_num_evenorodd = num%2==0?(num==0?"Zero is netheir odd nor even":"Its an even number"):"Its an odd number";
     System.out.println(Is_num_evenorodd);
     }
